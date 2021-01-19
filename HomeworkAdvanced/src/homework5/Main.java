46ab21f68d18e34afd18101b14adc06be6d2d652
IlyaK <ilja.kasatckin@yandex.ru>
2021-01-19 21:17:53 +0300

    Во втором методе сделал long a = System.currentTimeMillis(); перед созданием массивов. Оставил один метод calculate(параметр массив, параметр half). Сделал метод calculateOneThread, который возвращает переписанный массив и главный метод перезаписывает массив arr.

--- HomeworkAdvanced/src/homework5/Main.java
+++ HomeworkAdvanced/src/homework5/Main.java
@@ -4,19 +4,20 @@ import java.util.Arrays;
 
 public class Main {
     static final int SIZE = 10_000_000;
-    static final int HALF = SIZE / 2;
-    static final float[] arr = new float[SIZE];
 
     public static void main(String[] args) {
-
-        Arrays.fill(arr, 1f);
+        int half = SIZE / 2;
+        float[] arr = new float[SIZE];
         MassiveWork massiveWork = new MassiveWork();
 
+        Arrays.fill(arr, 1f);
         long a = System.currentTimeMillis();
-        System.arraycopy(massiveWork.calculate(arr), 0, arr, 0, arr.length);
+        System.arraycopy(massiveWork.calculateOneThread(arr), 0, arr, 0, arr.length);
         System.out.println("Время работы в одном потоке " + (System.currentTimeMillis() - a));
 
         Arrays.fill(arr, 1f);
-        massiveWork.calculateMulThread(arr, HALF);
+        long b = System.currentTimeMillis();
+        System.arraycopy(massiveWork.calculateMulThread(arr, half), 0, arr, 0, arr.length);
+        System.out.println("Время работы в двух потоках " + (System.currentTimeMillis() - b));
     }
 }
--- HomeworkAdvanced/src/homework5/MassiveWork.java
+++ HomeworkAdvanced/src/homework5/MassiveWork.java
@@ -2,46 +2,41 @@ package homework5;
 
 public class MassiveWork {
 
-    protected float[] calculate(float[] arr1) {
-        for (int i = 0; i < arr1.length; i++) {
-            arr1[i] = (float) (arr1[i] * Math.sin(0.2f + i / 5.) * Math.cos(0.2f + i / 5.) *
-                    Math.cos(0.4f + i / 2.));
+    private float[] calculate(float[] arr, int half) {
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (float) (arr[i] * Math.sin(0.2f + (i + half) / 5.) * Math.cos(0.2f + (i + half) / 5.) *
+                    Math.cos(0.4f + (i + half) / 2.));
         }
-        return arr1;
+        return arr;
     }
 
-    private float[] calculateSecondHalf(float[] arr2, int half) {
-        for (int i = 0; i < arr2.length; i++) {
-            arr2[i] = (float) (arr2[i] * Math.sin(0.2f + (i+half)  / 5.) * Math.cos(0.2f + (i+half) / 5.) *
-                    Math.cos(0.4f + (i+half) / 2.));
-        }
-        return arr2;
+    protected float[] calculateOneThread(float[] arr) {
+        System.arraycopy(calculate(arr, 0), 0, arr, 0, arr.length);
+        return arr;
     }
 
-    protected void calculateMulThread(float[] arr, int half){
+    protected float[] calculateMulThread(float[] arr, int half) {
         float[] arr1 = new float[half];
         float[] arr2 = new float[half];
-        long a = System.currentTimeMillis();
+
         System.arraycopy(arr, 0, arr1, 0, half);
         System.arraycopy(arr, half, arr2, 0, half);
 
-        Thread t1 = new Thread(()-> System.arraycopy(calculate(arr1), 0, arr1, 0, arr1.length));
-
-        Thread t2 = new Thread(()-> System.arraycopy(calculateSecondHalf(arr2, half), 0, arr2, 0, arr2.length));
+        Thread t1 = new Thread(() -> System.arraycopy(calculate(arr1, 0), 0, arr1, 0, arr1.length));
+        Thread t2 = new Thread(() -> System.arraycopy(calculate(arr2, half), 0, arr2, 0, arr2.length));
 
         t1.start();
         t2.start();
 
-        try{
+        try {
             t1.join();
             t2.join();
-        } catch (InterruptedException e){
+        } catch (InterruptedException e) {
             System.out.printf("Что-то не так в одном из потоков %s%n", e.getMessage());
         }
 
         System.arraycopy(arr1, 0, arr, 0, half);
         System.arraycopy(arr2, 0, arr, half, half);
-
-        System.out.println("Время работы в двух потоках " + (System.currentTimeMillis() - a));
+        return arr;
     }
 }
